05a02986300e2e18fe3e81dd37249ab0bc91dce5
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2025-12-08 19:17:51 -0300

    Update Product.java

--- src/main/java/com/pdds/domain/Product.java
+++ src/main/java/com/pdds/domain/Product.java
@@ -34,7 +34,7 @@ public class Product {
 
     @ElementCollection
     @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
-    @Column(name = "url")
+    @Column(name = "images")
     private List<String> images;
 
     public Product(String name, double price, int stock, List<String> images) {
