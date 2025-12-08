60f5fd074692d73f288b136913e7fb32085e3425
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2025-12-08 19:26:30 -0300

    Update Product.java

--- src/main/java/com/pdds/domain/Product.java
+++ src/main/java/com/pdds/domain/Product.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -32,10 +33,10 @@ public class Product {
     @Column(name = "stock")
     private int stock;
 
-    @ElementCollection
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
     @Column(name = "images")
-    private List<String> images;
+    private List<String> images = new ArrayList<>();
 
     public Product(String name, double price, int stock, List<String> images) {
         this.name = name;
