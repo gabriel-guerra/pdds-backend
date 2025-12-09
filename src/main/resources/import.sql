745a28e65052dd6598c33d7bc22a73f92ad26973
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2025-12-09 13:17:50 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ INSERT INTO users (id, email, password, full_name, role) VALUES (1, 'admin@examp
 INSERT INTO users (id, email, password, full_name, role) VALUES (2, 'user@example.com', '$2a$12$3aAThWbk5pq1//KAPIngT.1I6w1OpqjlJmsJVfZaBY4gli4INsTP2', 'User', 1);
 
 INSERT INTO products (id, name, price, stock) VALUES (1, 'PlayStation 5', 5000.0, 1000);
-INSERT INTO product_images (product_id, images) VALUES (1, 'https://png.pngtree.com/png-vector/20250221/ourlarge/pngtree-top-quality-playstation-5-console-isolated-png-image_15514648.png');
-INSERT INTO product_images (product_id, images) VALUES (1, 'https://www.citypng.com/public/uploads/preview/playstation-5-sony-ps5-side-view-with-controller-701751695142204cgnpwtopeb.png?v=2025112400');
 INSERT INTO product_images (product_id, images) VALUES (1, 'https://wallpapers.com/images/featured/ps5-console-png-ywbv2gv3gfw23o3w.jpg');
+INSERT INTO product_images (product_id, images) VALUES (1, 'https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$   ');
+INSERT INTO product_images (product_id, images) VALUES (1, 'https://applebygames.co.nz/wp-content/uploads/2024/10/IMG_9391-Photoroom-Medium.png');
 
 INSERT INTO products (id, name, price, stock) VALUES (5, 'PlayStation 4', 3000.0, 5000);
 INSERT INTO product_images (product_id, images) VALUES (5, 'https://gmedia.playstation.com/is/image/SIEPDC/ps4-product-thumbnail-01-en-14sep21?$facebook$');
@@ -14,17 +14,17 @@ INSERT INTO product_images (product_id, images) VALUES (5, 'https://upload.wikim
 INSERT INTO products (id, name, price, stock) VALUES (2, 'Nintendo Switch', 2500.0, 500);
 INSERT INTO product_images (product_id, images) VALUES (2, 'https://www.pngall.com/wp-content/uploads/2/Nintendo-Switch-PNG.png');
 INSERT INTO product_images (product_id, images) VALUES (2, 'https://static.vecteezy.com/system/resources/thumbnails/013/007/615/small/set-of-console-game-device-handheld-portable-free-png.png');
-INSERT INTO product_images (product_id, images) VALUES (2, 'https://e7.pngegg.com/pngimages/469/643/png-clipart-nintendo-switch-pokemon-red-and-blue-super-mario-odyssey-video-game-consoles-nintendo-blue-electronics.png');
+INSERT INTO product_images (product_id, images) VALUES (2, 'https://static.vecteezy.com/system/resources/thumbnails/013/007/607/small/set-of-console-game-device-handheld-portable-free-png.png');
 
 INSERT INTO products (id, name, price, stock) VALUES (3, 'Xbox Series X', 4000.0, 3500);
 INSERT INTO product_images (product_id, images) VALUES (3, 'https://cms-assets.xboxservices.com/assets/68/a0/68a0e50d-0d13-42b1-8498-e55cef8a9133.png?n=642227_Hero-Gallery-0_A2_857x676.png');
-INSERT INTO product_images (product_id, images) VALUES (3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQwqyztTdB79KS8TykE7H8MYknq62RpQAw-w&s');
+INSERT INTO product_images (product_id, images) VALUES (3, 'https://cms-assets.xboxservices.com/assets/bc/40/bc40fdf3-85a6-4c36-af92-dca2d36fc7e5.png?n=642227_Hero-Gallery-0_A1_857x676.png');
 INSERT INTO product_images (product_id, images) VALUES (3, 'https://pngimg.com/d/xbox_PNG101378.png');
 
 INSERT INTO products (id, name, price, stock) VALUES (4, 'iPhone 17 Pro Max', 15000.0, 200);
-INSERT INTO product_images (product_id, images) VALUES (4, 'https://www.pngall.com/wp-content/uploads/20/iPhone-17-Pro-Max-Concept-Art-PNG-thumb.png');
+INSERT INTO product_images (product_id, images) VALUES (4, 'https://revendo.ch/cdn/shop/files/apple-iphone-17-pro-apple-a19-pro-3-nm-17-pro-cosmic-orange-orange-guenstig-gebraucht-kaufen-00_grande.png?v=1758094341');
 INSERT INTO product_images (product_id, images) VALUES (4, 'https://pngdownloads.wordpress.com/wp-content/uploads/2025/09/apple-iphone-17-pro-png-1.jpg?w=640';
-INSERT INTO product_images (product_id, images) VALUES (4, 'https://reidocelular.com.br/wp-content/uploads/2025/12/iphone-17-pro-4.png');
+INSERT INTO product_images (product_id, images) VALUES (4, 'https://www.mozillion.com/storage/product_model_images/1757672591_iPhone%2017%20Pro%20-%20Cosmic%20Orange%20-%20Side.png');
 
 
 
