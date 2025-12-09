c638aebfe2a0529d7c150b1f398aef6546e51f3b
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2025-12-09 14:24:32 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -22,8 +22,8 @@ INSERT INTO product_images (product_id, images) VALUES (3, 'https://cms-assets.x
 INSERT INTO product_images (product_id, images) VALUES (3, 'https://pngimg.com/d/xbox_PNG101378.png');
 
 INSERT INTO products (id, name, price, stock) VALUES (4, 'iPhone 17 Pro Max', 15000.0, 200);
-INSERT INTO product_images (product_id, images) VALUES (4, 'https://www.mozillion.com/storage/product_model_images/1757672591_iPhone%2017%20Pro%20-%20Cosmic%20Orange%20-%20Side.png');
 INSERT INTO product_images (product_id, images) VALUES (4, 'https://revendo.ch/cdn/shop/files/apple-iphone-17-pro-apple-a19-pro-3-nm-17-pro-cosmic-orange-orange-guenstig-gebraucht-kaufen-00_grande.png?v=1758094341');
+INSERT INTO product_images (product_id, images) VALUES (4, 'https://www.mozillion.com/storage/product_model_images/1757672591_iPhone%2017%20Pro%20-%20Cosmic%20Orange%20-%20Side.png');
 INSERT INTO product_images (product_id, images) VALUES (4, 'https://pngimg.com/uploads/iphone17/iphone17_PNG13.png');
 
 
